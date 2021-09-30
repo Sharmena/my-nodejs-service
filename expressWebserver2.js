f34c09d38e8bbb724bbee072ead9e031355a6266
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2021-09-30 18:46:08 -0400

    Update expressWebserver2.js

--- expressWebserver2.js
+++ expressWebserver2.js
@@ -3,4 +3,13 @@ var app = express();
  
 app.use(express.static('www'))
  
-app.listen(3000);
+// app.listen(3000);
+// Listen to the App Engine-specified port, or 8080 otherwise
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+// [END app]
+
+module.exports = app;
